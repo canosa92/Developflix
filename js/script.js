5ef5435fe775420977e110b6639af2cebba63e9e
Adrian Canosa <101671780+canosa92@users.noreply.github.com>
2023-11-14 21:29:37 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,4 +63,26 @@ for (let i=0;i<peliculasAventuras.length;i++){
     contenedor.appendChild(texto);
     divAventuras.appendChild(contenedor)
 }
+/*const mostrarPeliculaPorGenero = (generoId, contenedorId) => {
+  const contenedor = document.getElementById(contenedorId)
+  const peliculaGenero = peliculas.filter((pelicula) => pelicula.genre_ids.includes(generoId))
 
+  peliculaGenero.forEach(pelicula => {
+    let templatePelis =`
+    <div class="pelicula">
+      <img src="https://image.tmdb.org/t/p/w200/${pelicula.backdrop_path}" alt="${pelicula.title}">
+      <h3>${pelicula.title}</h3>
+    </div>
+    `
+    contenedor.innerHTML += templatePelis
+  })
+
+}
+
+
+mostrarPeliculaPorGenero(28, "genero-28")
+mostrarPeliculaPorGenero(53, "genero-53")
+mostrarPeliculaPorGenero(12, "genero-12")
+
+
+*/
